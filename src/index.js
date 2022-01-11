52d8032ef969a5661011444f110a02f17a2d13dc
blvkroot <hsalukamba17@gmail.com>
2022-01-11 18:14:13 +0100

    Listando extrato da bancário por data

--- src/index.js
+++ src/index.js
@@ -22,15 +22,16 @@ function verifyIfCPFAccountExists(request, response, next) {
 }
 
 function getBalance(statement) {
-    const balance = statement.reduce((acc, operation) => {
-        if(operation.type === 'credit') {
-            return acc + operation.amount;
-        } {
-            return acc - operation.amount;
-        }
-    }, 0);
+  const balance = statement.reduce((acc, operation) => {
+    if (operation.type === "credit") {
+      return acc + operation.amount;
+    }
+    {
+      return acc - operation.amount;
+    }
+  }, 0);
 
-    return balance;
+  return balance;
 }
 
 app.post("/account", (request, response) => {
@@ -58,41 +59,56 @@ app.get("/statement", verifyIfCPFAccountExists, (request, response) => {
   return response.json(customer.statement);
 });
 
-app.post('/deposit', verifyIfCPFAccountExists, (request, response) => {
-    const { description, amount } = request.body;
-    const { customer } = request;
+app.post("/deposit", verifyIfCPFAccountExists, (request, response) => {
+  const { description, amount } = request.body;
+  const { customer } = request;
 
-    const statementOperation = {
-        description,
-        amount,
-        created_at: new Date(),
-        type: "credit"
-    }
+  const statementOperation = {
+    description,
+    amount,
+    created_at: new Date(),
+    type: "credit",
+  };
 
-    customer.statement.push(statementOperation);
+  customer.statement.push(statementOperation);
 
-    return response.status(201).send();
+  return response.status(201).send();
 });
 
-app.post('/withdraw', verifyIfCPFAccountExists, (request, response) => {
-    const { amount } = request.body;
-    const { customer } = request;
+app.post("/withdraw", verifyIfCPFAccountExists, (request, response) => {
+  const { amount } = request.body;
+  const { customer } = request;
 
-    const balance = getBalance(customer.statement);
+  const balance = getBalance(customer.statement);
 
-    if(balance < amount) {
-        return response.status(400).json({ error: 'Insufficient funds!'})
-    }
+  if (balance < amount) {
+    return response.status(400).json({ error: "Insufficient funds!" });
+  }
 
-    const statementOperation = {
-        amount,
-        created_at: new Date(),
-        type: "debit"
-    }
+  const statementOperation = {
+    amount,
+    created_at: new Date(),
+    type: "debit",
+  };
 
-    customer.statement.push(statementOperation);
+  customer.statement.push(statementOperation);
 
-    return response.status(201).send();
+  return response.status(201).send();
+});
+
+app.get("/statement/date", verifyIfCPFAccountExists, (request, response) => {
+  const { customer } = request;
+  const { date } = request.query;
+
+  const dateFormat = new Date(date + " 00:00");
+
+  const statement = customer.statement.filter(
+    (statement) =>
+      statement.created_at.toDateString() ===
+      new Date(dateFormat).toDateString()
+  );
+
+  return response.json(statement);
 });
 
 app.listen(port, () => console.log(`Server running 🚀 🚀!`));
